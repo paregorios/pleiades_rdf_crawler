946682aa8a72af12d3b5dda4d722f13df38c3ca9
Tom Elliott <ipse@paregorios.org>
2023-03-16 15:58:05 -0500

    get data for a pleiades URI and turn it into an rdflib graph

--- play1.py
+++ play1.py
@@ -4,7 +4,10 @@ play1
 
 from airtight.cli import configure_commandline
 import logging
+from rdflib import Graph, Namespace, URIRef
+from urllib.parse import urlparse
 from validators import url as valid_uri
+from webiquette.webi import Webi
 
 logger = logging.getLogger(__name__)
 
@@ -32,13 +35,60 @@ POSITIONAL_ARGUMENTS = [
     ["start_id", str, "Pleiades URI to start with"]
 ]
 BASE_URI = "https://pleiades.stoa.org/places/"
+ns_pleiades_relationship_types = Namespace(
+    "https://pleiades.stoa.org/vocabularies/relationship-types/"
+)
+ns_pleiades_places = Namespace("https://pleiades.stoa.org/places/vocab#")
+
+
+def get_ttl(webi, puri) -> str:
+    ### download and return RDF/TTL for puri as text
+    r = webi.get(puri + "/turtle")
+    if r.status_code != 200:
+        r.raise_for_status()
+    return r.text
+
+
+def get_json(webi, puri) -> dict:
+    ### download and return JSON for puri as a python dict
+    r = webi.get(puri + "/json")
+    if r.status_code != 200:
+        r.raise_for_status()
+    return r.json()
+
+
+def get_place(webi, puri) -> Graph:
+    ### download and parse data about the place at puri and return it as an RDFLIB Graph
+    ttl = get_ttl(webi, puri)
+    g = Graph()
+    g.parse(data=ttl, format="turtle")
+    logger.debug(f"RDF for {puri} has {len(g)} triples.")
+    # annoyingly, pleiades RDF doesn't have the connections from the database, so we have to build and add them from the JSON
+    j = get_json(webi, puri)
+    connections = j["connections"]
+    logger.debug(f"JSON for {puri} has {len(connections)} connections.")
+    for c in connections:
+        g.add((URIRef(puri), URIRef(c["connectionTypeURI"]), URIRef(c["connectsTo"])))
+    logger.debug(f"Modified RDF for {puri} now has {len(g)} triples.")
+    return g
+
+
+def get_web_interface() -> Webi:
+    ### instantiate and return a "webi" web interface object for the Pleiades website
+    headers = {
+        "User-Agent": "Pleiades4Sebs2023/0.1",
+        "from": "pleiades.admin@nyu.edu",
+    }
+    return Webi(netloc=urlparse(BASE_URI).netloc, headers=headers)
 
 
 def raise_invalid_uri(v):
+    ### raise a value error for an invalid Pleiades URI
     raise ValueError(f"Invalid Pleiades URI or ID: {v}")
 
 
 def validate_id(v):
+    ### validate and coerce if necessary a Pleiades place ID or URI
     id = v
     while True:
         if not (valid_uri(id)):
@@ -60,6 +110,10 @@ def main(**kwargs):
     """
     start_uri = validate_id(kwargs["start_id"])
     logger.debug(f"start_uri: {start_uri}")
+
+    webi = get_web_interface()
+
+    pg = get_place(webi, start_uri)
 
 
 if __name__ == "__main__":
